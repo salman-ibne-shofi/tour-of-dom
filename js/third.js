4bb26277216face8019d46025b8b73d5f34dcd1b
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-02-12 19:10:29 +0600

    dom summary

---
+++ js/third.js
@@ -0,0 +1,14 @@
+const main = document.getElementById('main-container');
+const section = document.createElement('section');
+section.innerHTML = `
+<h1> MY dynamic section</h1>
+<p> Extra text added inside paragraph</p>
+<ul>
+    <li>first item</li>
+    <li>first item</li>
+    <li>first item</li>
+    <li>first item</li>
+</ul>
+`
+
+main.appendChild(section);
